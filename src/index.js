16e1c090bee4090f82d66533f8d3b741e5b5e1ed
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-04-21 15:40:31 +0530

    react router and bootstrap installed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//react bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//react router 5
+import {BrowserRouter} from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
