4df200013df680b82b576d6827dbfa5ec5401727
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-04-21 22:42:27 +0530

    FAQ and Home component implemented

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,5 +1,55 @@
-const FAQ = () => {
+import { Accordion, Container, Card, Button } from "react-bootstrap";
 
-}
+const FAQ = () => {
+  return (
+    <Container style={{ marginTop: "20px" }}>
+      <h6>Frequently Asked Questions</h6>
+      <br />
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>How to book Taxi</Accordion.Header>
+          <Accordion.Body>
+            You can click on Book button at the top right corner and fill the
+            details.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br />
+      <Accordion>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            When I will get booking confirmation
+          </Accordion.Header>
+          <Accordion.Body>
+            As soon as we will receive your request within 5 to 10 min, we will
+            send you the booking confirmation.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br />
+      <Accordion>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Can make the advance payment</Accordion.Header>
+          <Accordion.Body>
+            Yes, You have got the options to pay upfront before the start of
+            journey or pay at the end.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br />
+      <Accordion>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            Is it safe to travel with speed taxi post pandemic
+          </Accordion.Header>
+          <Accordion.Body>
+            Yes, it’s safe. We’re are continuously working to help keep those
+            who rely on our platform healthy and safe for everyone{" "}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
+  );
+};
 
 export default FAQ;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,58 @@
-const Home = () => {
+import { Carousel, Container } from "react-bootstrap";
+import taxiLogo from '../assets/logo.jpg'
+import taxi2 from '../assets/taxi2.jpg'
+import taxi3 from '../assets/taxi3.jpg'
+import taxi1 from '../assets/taxi1.jpg'
 
+
+const Home = () => {
+    return(
+      // <Container style={{marginTop: '100px'}}>
+        <Carousel variant="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={taxiLogo}
+          height='500px'
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Welcome to speed taxi</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-50"
+          src={taxi2}
+          height='500px'
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-50"
+          src={taxi1}
+          height='500px'
+          alt="Third slide"
+          style={{float: 'right'}}
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    // </Container>
+    )
 }
 
 export default Home;
